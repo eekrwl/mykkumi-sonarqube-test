b87994fecd495befaac90b9235132241ba31ac5d
eekrwl <wani9909@naver.com>
2024-06-15 11:29:14 +0900

    first commit
    gitignore에 .DS_Store 추가
    /v1/home에 Hello World

---
+++ src/main/java/com/swmarastro/mykkumiserver/MykkumiServerApplication.java
@@ -0,0 +1,18 @@
+package com.swmarastro.mykkumiserver;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykkumiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MykkumiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swmarastro/mykkumiserver/MykkumiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swmarastro.mykkumiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykkumiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
