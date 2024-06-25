3b665a091fb3ca2bf141e8d2d25cc11944c9f8bf
안채완 <59831262+eekrwl@users.noreply.github.com>
2024-06-25 18:35:54 +0900

    Revert "KKUMI-16 get home banners"

--- src/main/java/com/swmarastro/mykkumiserver/MykkumiServerApplication.java
+++ src/main/java/com/swmarastro/mykkumiserver/MykkumiServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MykkumiServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(MykkumiServerApplication.class, args);
     }
+
 }
--- src/main/java/com/swmarastro/mykkumiserver/domain/banner/BannerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.swmarastro.mykkumiserver.domain.banner;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BannerRepository extends JpaRepository<Banner, Long> {
-}
