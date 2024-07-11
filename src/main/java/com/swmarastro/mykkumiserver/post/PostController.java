e6219eda3b5f82ed113c37d2f2f72a8ad191d539
eekrwl <wani9909@naver.com>
2024-07-11 21:31:35 +0900

    KKUMI-39 [CHORE] #17 사용하지 않는 import 제거

--- src/main/java/com/swmarastro/mykkumiserver/post/PostController.java
+++ src/main/java/com/swmarastro/mykkumiserver/post/PostController.java
@@ -2,14 +2,9 @@ package com.swmarastro.mykkumiserver.post;
 
 import com.swmarastro.mykkumiserver.post.dto.PostListResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
