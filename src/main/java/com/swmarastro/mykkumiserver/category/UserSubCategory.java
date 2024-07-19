2e5312c2a671f491a982e52ec157bbc2d987c12d
eekrwl <wani9909@naver.com>
2024-07-19 17:01:02 +0900

    KKUMI-38 [FEAT] #23 카테고리 생성 및 업데이트 기능
    User 생성시마다 UserSubCategory도 하나씩 생성예정
    User가 선택한 카테고리들은 1,2,3과 같이 ,로 구분한 문자열로 저장
    해당 정보들이 따로 돌아다닐 일이 없고 여러 레코드로 분리했을 때 더복잡해 질 것 같습니다.

--- src/main/java/com/swmarastro/mykkumiserver/category/UserSubCategory.java
+++ src/main/java/com/swmarastro/mykkumiserver/category/UserSubCategory.java
@@ -2,10 +2,20 @@ package com.swmarastro.mykkumiserver.category;
 
 import com.swmarastro.mykkumiserver.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserSubCategory {
 
     @Id
@@ -13,11 +23,32 @@ public class UserSubCategory {
     @Column(name = "user_sub_category")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "sub_category_id", nullable = false)
-    private SubCategory subCategory;
+    private String subCategory;
+
+    public static String listToString(List<Long> categoryIds) {
+        return categoryIds.stream().map(Object::toString).collect(Collectors.joining(","));
+    }
+
+    public static List<Long> stringToList(String categoryString) {
+        return Arrays.stream(categoryString.split(","))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
+    }
+
+    public String updateSubCategory(List<Long> categoryIds) {
+        String categoryStr = listToString(categoryIds);
+        this.subCategory = categoryStr;
+        return categoryStr;
+    }
+
+    public static UserSubCategory of(User user) {
+        return UserSubCategory.builder()
+                .user(user)
+                .subCategory(null)
+                .build();
+    }
 }
---
+++ src/main/java/com/swmarastro/mykkumiserver/category/UserSubCategoryRepository.java
@@ -0,0 +1,6 @@
+package com.swmarastro.mykkumiserver.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserSubCategoryRepository extends JpaRepository<UserSubCategory, Long> {
+}
